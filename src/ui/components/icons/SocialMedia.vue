bad24e5a119b8177b745b6e4c2c8d650a5c23a6a
Armando Alamilla Cuellar <armando123alamilla@gmail.com>
2022-03-14 02:00:08 -0600

    added base config for SocialMedia icon component

---
+++ src/ui/components/icons/SocialMedia.vue
@@ -0,0 +1,19 @@
+<template>
+  <nuxt-link :to="iconUrl">icon</nuxt-link>
+</template>
+
+<script>
+export default {
+  name: 'SocialMedia',
+  props: {
+    className: {
+      type: String,
+      default: '',
+    },
+    iconUrl: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
