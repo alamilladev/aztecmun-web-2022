0137a54b028501859eb72bea049d4092914f5e63
Armando Alamilla Cuellar <armando123alamilla@gmail.com>
2022-03-16 12:35:20 -0600

    added sttistics page

---
+++ src/ui/pages/statistics.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="statistics">
+    <div class="container">
+      <h1 class="primary page-title">
+        Estádisticas de registro de delegados para comités
+      </h1>
+
+      <div class="statistics-table-container">grafico de estadisticas</div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'StatisticsPage',
+  layout: 'HomeLayout',
+  head: {
+    title: 'AztecMUN 2022 | Estadísticas',
+  },
+};
+</script>
+
+<style lang="scss">
+.statistics {
+  .statistics-table-container {
+    overflow-x: auto;
+  }
+}
+</style>
